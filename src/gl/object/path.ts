36d69a71c8a5a88bcca2a92db5b342b1df620e6f
kalenkevich <alexey.kalenkevich@gmail.com>
2023-04-16 22:24:38 -0700

    Render lines with triangles!

--- src/gl/object/path.ts
+++ src/gl/object/path.ts
@@ -8,6 +8,15 @@ export interface GlPathProps extends GlObjectProps {
 export class GlPath extends GlProgram {
   protected points: v2[];
 
+  protected segmentInstanceGeometry = [
+    [0, -0.5],
+    [1, -0.5],
+    [1,  0.5],
+    [0, -0.5],
+    [1,  0.5],
+    [0,  0.5]
+  ];
+
   constructor(gl: WebGLRenderingContext, props: GlPathProps) {
     super(gl, props);
 
@@ -16,113 +25,65 @@ export class GlPath extends GlProgram {
 
   public get primitiveType(): GLenum {
     return this.gl.TRIANGLES;
-    //return this.gl.TRIANGLE_STRIP;
-    //return this.gl.LINE_STRIP;
+    // return this.gl.TRIANGLE_STRIP;
+    // return this.gl.LINE_STRIP;
+    // return this.gl.LINES;
   }
 
-  // public getBufferAttrs(): Record<string, any> {
-  //   const points = [];
-  //   for (let i = 1; i < this.points.length; i++) {
-  //     points.push(this.points[i - 1]);
-  //     points.push(this.points[i]);
-  //   }
-  //   return {
-  //     a_position: {
-  //       numComponents: 2,
-  //       data: points.flatMap(p => p),
-  //     },
-  //   };
-  // }
-
+  // Render basic lines with triangles.
   public getVertexShaderSource(...args: any[]): string {
     return `
       attribute vec2 a_position;
-      attribute vec3 a_miter_position;
-      attribute vec2 point_a, point_b, point_c;
+      attribute vec2 point_a, point_b;
       uniform float u_width;
       uniform vec2 u_resolution;
       uniform mat3 u_matrix;
-      uniform mat4 u_projection;
       
       void main() {
-        // // Apply tranlation, rotation and scale.
-        // // vec3 position = (u_matrix * a_position);
-        // vec2 position = (u_matrix * vec3(a_position, 1)).xy;
-        // // vec3 position = (u_matrix * a_position);
+        vec2 xBasis = point_b - point_a;
+        vec2 yBasis = normalize(vec2(-xBasis.y, xBasis.x));
+        vec2 point = point_a + xBasis * a_position.x + yBasis * u_width * a_position.y;
+
+        // Apply tranlation, rotation and scale.
+        vec2 position = (u_matrix * vec3(point, 1)).xy;
         
-        // // Apply resolution.
-        // vec2 zeroToOne = position / u_resolution;
-        // vec2 zeroToTwo = zeroToOne * 2.0;
-        // vec2 clipSpace = zeroToTwo - 1.0;
+        // Apply resolution.
+        vec2 zeroToOne = position / u_resolution;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
 
-        // gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-        // // gl_Position = u_projection * vec4(position, 1);
-
-        vec2 tangent = normalize(normalize(point_c - point_b) + normalize(point_b - point_a));
-        vec2 miter = vec2(-tangent.y, tangent.x);
-        vec2 ab = point_b - point_a;
-        vec2 cb = point_b - point_c;
-        vec2 abNorm = normalize(vec2(-ab.y, ab.x));
-        vec2 cbNorm = -normalize(vec2(-cb.y, cb.x));
-
-        float sigma = sign(dot(ab + cb, miter));
-
-        vec2 p0 = 0.5 * u_width * sigma * (sigma < 0.0 ? abNorm : cbNorm);
-        vec2 p1 = 0.5 * miter * sigma * u_width / dot(miter, abNorm);
-        vec2 p2 = 0.5 * u_width * sigma * (sigma < 0.0 ? cbNorm : abNorm);
-
-        vec2 point = point_b + a_miter_position.x * p0 + a_miter_position.y * p1 + a_miter_position.z * p2;
-        gl_Position = u_projection * vec4(point, 0, 1);
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
       }
     `;
   }
 
   public getBufferAttrs(): Record<string, any> {
-    const instanceMiterJoin = [
-      [0, 0, 0],
-      [1, 0, 0],
-      [0, 1, 0],
-      [0, 0, 0],
-      [0, 1, 0],
-      [0, 0, 1]
-    ];
-    let points = [];
-
-    for (let i = 1; i < this.points.length; i++) {
-      points.push(this.points[i - 1]);
-      points.push(this.points[i]);
-    }
-    points = points.flatMap(p => p);
+    const points = this.points.flatMap(p => p);
 
     return {
       a_position: {
         numComponents: 2,
-        data: points,
-      },
-      a_miter_position: {
-        numComponents: 3,
-        data: instanceMiterJoin,
+        data: this.segmentInstanceGeometry.flatMap(p => p),
         divisor: 0,
       },
       point_a: {
         numComponents: 2,
         data: points,
         divisor: 1,
-        offset: Float32Array.BYTES_PER_ELEMENT * 0
       },
       point_b: {
         numComponents: 2,
-        data: points,
+        data: points.slice(2),
         divisor: 1,
-        offset: Float32Array.BYTES_PER_ELEMENT * 2
       },
-      point_c: {
-        numComponents: 2,
-        data: points,
-        divisor: 1,
-        offset: Float32Array.BYTES_PER_ELEMENT * 4
-      },
-      //count: instanceMiterJoin.length,
+    };
+  }
+
+  public getDrawBufferInfoOptions(): { offset?: number; vertexCount?: number; instanceCount?: number;} {
+    return {
+      offset: 0, // offset
+      vertexCount: this.segmentInstanceGeometry.length, // num vertices per instance
+      instanceCount: this.points.length - 1, // num instances
     };
   }
 }
