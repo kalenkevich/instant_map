fdbed2465af8fed6e37d143ab783b77a58165889
Alexey Kalenkevich <alexey.kalenkevich@gmail.com>
2023-10-19 00:42:03 -0700

    Cover style utils with tests (#17)
    * Add tests for styles_utils.

--- src/map/styles/styles_utils.ts
+++ src/map/styles/styles_utils.ts
@@ -2,12 +2,14 @@ import { DataLayerStyle, FeatureStyle, FeatureStyleType } from './styles';
 import { ContextLike } from './style_statement';
 import { compileStatement, isStatement } from './style_statement_utils';
 
-export function compileLayerStyle(style: DataLayerStyle, feature: ContextLike): DataLayerStyle {
+export function compileLayerStyle(style: DataLayerStyle, context: ContextLike): DataLayerStyle {
   const newStyleObject: Record<string, any> = {};
 
   for (const [key, value] of Object.entries(style)) {
-    if (isFeatureStyle(value)) {
-      newStyleObject[key] = compileFeatureStyle(value, feature);
+    if (key === 'background' && isFeatureStyle(value)) {
+      newStyleObject[key] = compileFeatureStyle(value, context);
+    } else if (isStatement(value)) {
+      newStyleObject[key] = compileStatement(value, context);
     } else {
       newStyleObject[key] = value;
     }
@@ -16,26 +18,33 @@ export function compileLayerStyle(style: DataLayerStyle, feature: ContextLike): 
   return newStyleObject as DataLayerStyle;
 }
 
-export function compileFeatureStyle(paint: FeatureStyle, feature: ContextLike): FeatureStyle {
-  const newPaintObject: Record<string, any> = {};
+export function compileFeatureStyle(featureStyle: FeatureStyle, context: ContextLike): FeatureStyle {
+  const newStyleObject: Record<string, any> = {};
 
-  for (const [key, value] of Object.entries(paint)) {
-    if (isStatement(value)) {
-      newPaintObject[key] = compileStatement(value, feature);
+  for (const [key, value] of Object.entries(featureStyle)) {
+    if (isFeatureStyle(value)) {
+      newStyleObject[key] = compileFeatureStyle(value, context);
+    } else if (isStatement(value)) {
+      newStyleObject[key] = compileStatement(value, context);
     } else {
-      newPaintObject[key] = value;
+      newStyleObject[key] = value;
     }
   }
 
-  return newPaintObject as FeatureStyle;
+  return newStyleObject as FeatureStyle;
 }
 
-export function isFeatureStyle(paint: FeatureStyle): boolean {
+export function isFeatureStyle(featureStyle: unknown): boolean {
+  if (!featureStyle || !(featureStyle as FeatureStyle).type) {
+    return false;
+  }
+
   return [
+    FeatureStyleType.point,
     FeatureStyleType.line,
     FeatureStyleType.polygon,
     FeatureStyleType.text,
     FeatureStyleType.image,
     FeatureStyleType.background,
-  ].includes(paint.type);
+  ].includes((featureStyle as FeatureStyle).type);
 }
